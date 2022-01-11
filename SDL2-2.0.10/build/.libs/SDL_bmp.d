3b7729d46009436b4061b57389ab43c0d6e5493c
Steven <stevenjmiller13@gmail.com>
2022-01-11 09:51:59 -0700

    kivy test

---
+++ SDL2-2.0.10/build/.libs/SDL_bmp.d
@@ -0,0 +1,14 @@
+build/SDL_bmp.lo: \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/SDL_bmp.c \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/../SDL_internal.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/../dynapi/SDL_dynapi.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h include/SDL_hints.h include/SDL_stdinc.h \
+ include/SDL_config.h include/SDL_video.h include/SDL_pixels.h \
+ include/SDL_endian.h include/SDL_rect.h include/SDL_error.h \
+ include/SDL_rwops.h include/SDL_surface.h include/SDL_blendmode.h \
+ include/SDL_assert.h include/SDL_endian.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/SDL_pixels_c.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/video/SDL_blit.h \
+ include/SDL_cpuinfo.h include/SDL_surface.h
---
+++ SDL2-2.0.10/build/.libs/e_pow.d
@@ -0,0 +1,10 @@
+build/e_pow.lo: \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/e_pow.c \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/math_libm.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/../SDL_internal.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/../dynapi/SDL_dynapi.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/../dynapi/SDL_dynapi_overrides.h \
+ include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+ include/close_code.h \
+ /home/pi/Documents/Dish_Project/SDL2-2.0.10/src/libm/math_private.h \
+ include/SDL_endian.h include/SDL_stdinc.h include/SDL_config.h
